1e261f035ccc946699f2109246dc2e0f088bdb65
DIVYASHEK K <78641652+divyashek6267@users.noreply.github.com>
2021-05-01 16:10:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var database,canvas,backgroundImage;
+var gameState = 0;
+var playerCount;
+var form,player,game;
+
+function setup(){
+    database = firebase.database();
+    canvas = createCanvas(400,400);   
+
+    game = new Game();
+    game.getState();
+    game.start();
+
+}
+
+function draw(){
+
+}
+
